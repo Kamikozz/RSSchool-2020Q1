b00edae2255a61349f35560bf333d363a1d36b66
Alexey Shevchenko <kamikozz@mail.ru>
2020-04-14 20:46:59 +0300

    chore: add postcss, webpack, eslint, babel, editor configs

---
+++ english-for-kids/config/webpack.config.prod.js
@@ -0,0 +1,20 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const merge = require('webpack-merge');
+const baseWebpackConfig = require('./webpack.config.base');
+
+const prodWebpackConfig = merge(baseWebpackConfig, {
+  mode: 'production',
+  output: {
+    filename: `${baseWebpackConfig.externals.paths.assets}js/[name].[contenthash].js`,
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: `${baseWebpackConfig.externals.paths.assets}css/[name].[contenthash].css`,
+    }),
+  ],
+});
+
+module.exports = new Promise((resolve) => {
+  resolve(prodWebpackConfig);
+});
