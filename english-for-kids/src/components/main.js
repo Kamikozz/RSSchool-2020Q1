aa0737e877e015ae814fc5ad032db8e1a5cec291
Alexey Shevchenko <kamikozz@mail.ru>
2020-04-14 23:27:29 +0300

    feat: add cards data from task, change project structure of imgs & audios

--- english-for-kids/src/components/main.js
+++ english-for-kids/src/components/main.js
@@ -1,9 +1,11 @@
+import cardsData from '../js/cards.data';
 import trash from './trash';
 
 const add = (a, b) => a + b;
 
 console.log(add(2, 3));
 console.log('lol');
+console.log(cardsData);
 
 trash();
 
