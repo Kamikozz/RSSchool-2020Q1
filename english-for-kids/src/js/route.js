400a975d410339d35f6f171d4dbf345e9ede8160
Alexey Shevchenko <kamikozz@mail.ru>
2020-04-21 05:45:03 +0300

    refactor: restructure logics of route & router from markup loader into callback caller

--- english-for-kids/src/js/route.js
+++ english-for-kids/src/js/route.js
@@ -1,12 +1,12 @@
 class Route {
-  constructor(name, htmlName, defaultRoute) {
+  constructor(name, callback, defaultRoute) {
     try {
-      if (!name || !htmlName) {
-        throw new Error('name and htmlName params are required');
+      if (!name || !callback) {
+        throw new Error('name and callback params are required');
       }
 
       this.name = name;
-      this.htmlName = htmlName;
+      this.callback = callback;
       this.default = defaultRoute;
     } catch (e) {
       // console.log(e);
@@ -15,24 +15,19 @@ class Route {
 
   isActiveRoute() {
     const { hash } = window.location;
-    // console.log('WTF', hash);
-    // const parsedHash = hash.substring(1);
-    // console.log('Parsed: ', parsedHash);
-    // const parsedArr = parsedHash.split('/');
-    // console.log(parsedArr);
-    // const parsedFormatted = parsedArr.join('/');
-    // console.log('Joined: ', parsedFormatted);
-
     const parsedArr = hash.substring(1).split('/');
     const parsedFormatted = parsedArr.join('/');
 
     if (parsedArr.length === 3 && parsedArr[2] !== '') {
       const templatePath = this.name;
       const processedPath = templatePath.replace('{id}', parsedArr[2]);
-      // console.log(parsedFormatted, templatePath, processedPath);
       return processedPath === parsedFormatted;
     }
     return this.name === parsedFormatted;
+  }
+
+  goToRoute() {
+    this.callback();
   }
 }
 
--- english-for-kids/src/js/router.js
+++ english-for-kids/src/js/router.js
@@ -18,10 +18,9 @@ class Router {
   init() {
     this.initElements();
 
-    const { routes } = this.routes;
     const scope = this;
-    window.addEventListener('hashchange', () => scope.hasChanged(scope, routes));
-    this.hasChanged(this, routes);
+    window.addEventListener('hashchange', () => scope.hasChanged());
+    this.hasChanged();
   }
 
   initElements() {
@@ -29,50 +28,20 @@ class Router {
     this.mainElem = document.querySelector('.main-content .wrapper');
   }
 
-  hasChanged(scope) {
+  hasChanged() {
     const { hash } = window.location;
     const route = hash.length > 0
       ? this.routes.find((routeLocal) => routeLocal.isActiveRoute())
       : this.routes.find((routeLocal) => routeLocal.default);
-    // console.log(scope, hash, route);
+
     console.log('CURRENT ROUTE', route);
     if (route) {
-      scope.goToRoute(route.htmlName);
+      route.goToRoute();
     } else {
-      scope.goToRoute('404');
+      const notFoundComponent = new NotFound();
+      this.mainElem.innerHTML = notFoundComponent.getMarkup();
     }
   }
-
-  goToRoute(htmlName) {
-    console.log(this.routes, htmlName);
-    // const scope = this;
-    // const url = `views/${htmlName}`;
-    // scope.rootElem.innerHTML = this.responseText;
-
-    switch (htmlName) {
-      case '404': {
-        const notFoundComponent = new NotFound();
-        this.mainElem.innerHTML = notFoundComponent.getMarkup();
-        break;
-      }
-      default:
-        console.log('HEY, ADD SOME HANDLER TO ME!');
-        break;
-    }
-  }
-
-  // goToRoute(htmlName) {
-  //   const scope = this;
-  //   const url = `views/${htmlName}`;
-  //   const xhr = new XMLHttpRequest();
-  //   xhr.onreadystatechange = function requestCallback() {
-  //     if (this.readyState === 4 && this.status === 200) {
-  //       scope.rootElem.innerHTML = this.responseText;
-  //     }
-  //   };
-  //   xhr.open('GET', url, true);
-  //   xhr.send();
-  // }
 }
 
 export default Router;
