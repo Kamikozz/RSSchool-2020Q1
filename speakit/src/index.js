5cb76ac7bbadc184f2744627c30579b8316734fd
Alexey Shevchenko <kamikozz@mail.ru>
2020-04-26 21:42:34 +0300

    init: start speakit task with webpack4-template

---
+++ speakit/src/index.js
@@ -0,0 +1,8 @@
+
+// @babel/polyfill https://babeljs.io/docs/en/babel-polyfill/#usage-in-node-browserify-webpack
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+import './components/main';
+import './components/main.scss';
+import './components/main.css';
