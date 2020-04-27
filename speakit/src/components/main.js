1b778e83899ddc666bbefd228f91a351791a4df2
Alexey Shevchenko <kamikozz@mail.ru>
2020-04-27 03:58:13 +0300

    feat: add start page markup & style

--- speakit/src/components/main.js
+++ speakit/src/components/main.js
@@ -1,24 +1,3 @@
-import trash from './trash';
+import PageIntro from './page-intro/page-intro';
 
-const add = (a, b) => a + b;
-
-console.log(add(2, 3));
-console.log('lol');
-
-trash();
-
-// Lazy Loading
-// let print;
-
-// setTimeout(() => {
-//   import(/* webpackChunkName: 'trash' */ './trash').then(module => {
-//     print = module.default;
-//     // console.log(module);
-//     // module();
-//     print();
-//   });
-
-//   setTimeout(() => {
-//       print();
-//   }, 5000);
-// }, 5000);
+console.log(PageIntro);
