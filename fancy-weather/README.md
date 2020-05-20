20b2f4dd183d5b1974a2ae09edbd8d9edcda9194
Alexey Shevchenko <kamikozz@mail.ru>
2020-05-20 07:42:28 +0300

    init: start fancy-weather task from webpack4-template

---
+++ fancy-weather/README.md
@@ -0,0 +1,11 @@
+# Fancy Weather
+**Fancy Weather** - forecast application showing the weekly forecast, and where you can search for specific city's weather.
+Also it shows the latitude & longitude, interactive map of the input city; Speech Recognition API to control the search field by voice.
+It uses RESTful API's for retrieving data.
+
+---
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/a7561d9f-c25b-49c5-9b40-b637f955b6ff/deploy-status)](https://kamikozz-fancy-weather.netlify.app)
+
+#### [Deployed Site](https://kamikozz-fancy-weather.netlify.app)
+#### [Task Requirements](https://github.com/rolling-scopes-school/tasks/blob/master/tasks/fancy-weather.md)
---
+++ fancy-weather/config/webpack.config.base.js
@@ -0,0 +1,155 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const PATHS = {
+  src: path.join(__dirname, '../src'),
+  dist: path.join(__dirname, '../dist'),
+  assets: 'assets/',
+  static: 'static/',
+  config: 'config/',
+  fonts: 'fonts/',
+  img: 'img/',
+  audio: 'audio/',
+  icons: 'icons/',
+  views: 'views/',
+  components: 'components/',
+};
+
+module.exports = {
+  externals: {
+    paths: PATHS,
+  },
+  entry: {
+    app: `${PATHS.src}/index.js`,
+  },
+  output: {
+    path: PATHS.dist,
+    publicPath: '/',
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          name: 'vendors',
+          test: /[\\/]node_modules[\\/]/,
+          chunks: 'all',
+          enforce: true,
+        },
+      },
+    },
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: [
+        /(node_modules|dist|public)/,
+      ],
+      use: [
+        {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                '@babel/preset-env',
+                {
+                  exclude: ['transform-typeof-symbol'], // to prevent IE 11 error
+                },
+              ],
+            ],
+          },
+        },
+        {
+          loader: 'eslint-loader',
+        },
+      ],
+    }, {
+      test: /\.scss$/,
+      use: [
+        'style-loader',
+        MiniCssExtractPlugin.loader,
+        {
+          loader: 'css-loader',
+          options: {
+            sourceMap: true,
+          },
+        }, {
+          loader: 'postcss-loader',
+          options: {
+            sourceMap: true,
+            config: {
+              path: `${PATHS.config}postcss.config.js`,
+            },
+          },
+        }, {
+          loader: 'sass-loader',
+          options: {
+            sourceMap: true,
+          },
+        },
+      ],
+    }, {
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        MiniCssExtractPlugin.loader,
+        {
+          loader: 'css-loader',
+          options: {
+            sourceMap: true,
+          },
+        }, {
+          loader: 'postcss-loader',
+          options: {
+            sourceMap: true,
+            config: {
+              path: `${PATHS.config}postcss.config.js`,
+            },
+          },
+        },
+      ],
+    }, {
+      test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/i,
+      loader: 'file-loader',
+      options: {
+        name: '[name].[ext]',
+      },
+    }, {
+      test: /\.(png|jpe?g|gif|svg)$/i,
+      loader: 'file-loader',
+      options: {
+        name: '[name].[ext]',
+      },
+    }, {
+      test: /\.(wav|mp3)$/i,
+      loader: 'file-loader',
+      options: {
+        name: '[name].[ext]',
+      },
+    }],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: true, // to disable hash for some reasons (true - to boost loading)
+      template: `${PATHS.src}/index.html`,
+      filename: './index.html',
+      inject: true, // if true - to insert link & script tags into html
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: `${PATHS.src}/${PATHS.assets}${PATHS.img}`, to: `${PATHS.assets}${PATHS.img}` },
+        {
+          from: `${PATHS.src}/${PATHS.assets}${PATHS.fonts}`,
+          to: `${PATHS.assets}${PATHS.fonts}`,
+          globOptions: {
+            ignore: ['**/LICENSE*'],
+          },
+        },
+        { from: `${PATHS.src}/${PATHS.assets}${PATHS.icons}`, to: `${PATHS.assets}${PATHS.icons}` },
+        { from: `${PATHS.src}/${PATHS.assets}${PATHS.audio}`, to: `${PATHS.assets}${PATHS.audio}` },
+        { from: `${PATHS.src}/${PATHS.static}`, to: '' },
+      ],
+    }),
+  ],
+};
